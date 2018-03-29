cbdf9e61078d7964e4d9756843781149dc8dd5f9
Mike Chou <mchou@credera.com>
2018-03-29 09:40:10 -0500

    medication order

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -12,7 +12,7 @@
         var patient = smart.patient;
         var pt = patient.read();
         var obv = smart.patient.api.fetchAll({
-                    type: 'MedicationRequest',
+                    type: 'MedicationOrder',
                     query: {
                       code: {
                       }
